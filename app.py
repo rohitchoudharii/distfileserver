f813048c0d3233e95ec8c4102fbc788af4c47ecd
rohit <git@git.com>
2021-04-16 17:39:10 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,233 @@
+
+from flask import Flask, request
+from flask_json import FlaskJSON,JsonError, json_response, as_json
+import operationHandler as oph
+# from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+json = FlaskJSON(app)
+# cors = CORS(app)
+# app.config['CORS_HEADERS'] = 'application/json'
+
+
+@app.route('/')
+def index():
+    return json_response(msg = "test correct")
+
+@app.route('/save-file',methods = ['POST'])
+def save_file():
+    
+    '''
+    accepts form data
+    path does not matter
+    {
+    root: test,
+    path: '/',
+    content: 'rohit is the hero',
+    filename: 'text.txt'
+    }
+    '''
+    root = request.form.get('root')
+    path = request.form.get('path')
+    content = request.form.get('content')
+    filename = request.form.get('filename')
+    # root, path,content,filename
+    msg = oph.save_file(root=root, path=path,content=content,filename=filename)
+    if msg == 0:
+        return json_response(msg = "successful")
+    else:
+        return json_response(msg = "error")
+
+@app.route('/save-commit',methods = ['POST'])
+def save_commit():
+    '''
+    accepts json data
+    {
+        branch: branch_name,
+        root: "root name",
+        commit_msg: "first commit"
+        files: [{
+                    filename: "test.txt",
+                    content:"test"
+                },
+                {
+                    filename: "test.txt",
+                    content:"test"
+                }]
+    }
+    '''
+    payload = request.json
+    print(payload)
+
+    msg,code = oph.save_commit(payload = payload)
+    
+    return json_response(msg = msg, error=code)
+
+
+@app.route('/pull-server',methods = ['POST'])
+def pull_server():
+    '''
+    accepts form data
+    {
+    root: test,
+    project_name: test_project,
+    branch: username
+    }
+    '''
+    #root,project_name,branch
+    root = request.form.get('root')
+    project_name = request.form.get('project_name')
+    branch = request.form.get('branch')
+    print("app.py", root,branch,project_name)
+    hiry = oph.pull_editor(root = root, project_name= project_name,branch=branch)
+    return json_response(files = hiry)
+
+@app.route('/get-file',methods = ['POST'])
+def get_file():
+    '''
+    accepts form data
+    {
+    root: test,
+    path: '/',
+    filename: 'test.txt'
+    }
+    '''
+    #root,path,filename
+    root = request.form.get("root")
+    path = request.form.get("path")
+    filename = request.form.get("filename")
+    content = oph.get_file(root=root, path=path, filename = filename)
+    #print(content)
+    return json_response(content = content)
+
+@app.route('/hir',methods=['POST'])
+def get_hir():
+    '''
+    accepts form data
+    {
+        root:test
+    }
+    '''
+    root = request.form.get("root")
+    resp = oph.get_hir(root)
+    return json_response(hir=resp)
+
+@app.route('/delete-dir',methods = ['POST'])
+def del_dir():
+    '''
+    accepts form data
+    path does not matter
+    {
+    root: test,
+    path: '/',
+    dirname: 'test1'
+    }
+    '''
+    #root,path,new_dir
+    root = request.form.get("root")
+    path = request.form.get("path")
+    dirname = request.form.get("dirname")
+    hir = oph.delete_dir(root = root, path=path, dirname = dirname)
+    return json_response(hir = hir)
+
+@app.route('/delete-file',methods = ['POST'])
+def del_file():
+    '''
+    accepts form data
+    path does not matter
+    {
+    root: test,
+    path: '/',
+    filename: 'test1.php'
+    }
+    '''
+    #root,path,new_dir
+    root = request.form.get("root")
+    path = request.form.get("path")
+    filename = request.form.get("filename")
+    hir = oph.delete_file(root = root, path=path, filename = filename)
+    return json_response(hir = hir)
+
+
+@app.route('/make-dir',methods = ['POST'])
+def make_dir():
+    '''
+    accepts form data
+    path does not matter
+    {
+    root: test,
+    path: '/',
+    dirname: 'test1'
+    }
+    '''
+    #root,path,new_dir
+    root = request.form.get("root")
+    path = request.form.get("path")
+    new_dir = request.form.get("dirname")
+    hir,error = oph.make_dir(root = root, path=path, dir_name = new_dir)
+    return json_response(hir= hir,error = error)
+
+
+@app.route('/make-file',methods = ['POST'])
+def make_file():
+    '''
+    accepts form data
+    path matters
+    {
+    root: test,
+    path: '/test1',
+    filename: 'test1.txt'
+    }
+    '''
+    #root,path,filename
+    root = request.form.get("root")
+    path = request.form.get("path")
+    filename = request.form.get("filename")
+    file_hir,error = oph.make_file(root = root, path=path, filename = filename)
+    return json_response(hir = file_hir,error= error)
+
+@app.route('/make-repo',methods = ['POST'])
+def make_repo():
+    '''
+    accepts json data
+    {
+    projectname: 'test_project',
+    users = ["b1","b2","b3"]
+    }
+    '''
+    payload = request.json
+    msg = oph.make_repo(payload = payload)
+    return json_response(msg = msg)
+
+@app.route('/push-repo',methods = ['POST'])
+def push_repo():
+    '''
+    {
+        root: test,
+        branch: b1
+    }
+    '''
+    root = request.form.get("root")
+    branch = request.form.get("branch")
+    msg , code = oph.push_code(branch=branch, root=root)
+    return json_response(msg = msg, error= code)
+@app.route('/add-users',methods = ['POST'])
+def add_user():
+    '''
+    accepts json data
+    {
+        projectname : projectname,
+        base_branch : base_branch,
+        new_branchs : ['b1',b2,b3]
+    }
+    '''
+    payload = request.json
+    return oph.add_user(payload=payload)
+
+# @app.route('/merge',methods = ['POST'])
+# def merge_repo():
+#     pass
+
+
+if __name__ == "__main__":
+    app.run(port = 8090,debug=True)
---
+++ operationHandler.py
@@ -0,0 +1,212 @@
+
+from file_handler import FHandler
+from git_handler import GHandler
+#from paramiko import SSHClient
+import subprocess
+import time
+import json
+import os
+
+def push_code(root,branch):
+    fh = FHandler(dir_name = root)
+    gh = GHandler(fh.current_dir())
+    msg,code = gh.git_push(branch=branch)
+    if "fatal" in msg:
+        return "Error saving your file to the server",1
+    return "successfull saving all the files , now you can leave the editor",code
+
+
+def commit(msg,branch,root):
+    fh = FHandler(dir_name = root)
+    #fh.set_default_path()
+    gh = GHandler(cwd = fh.current_dir())
+    gh.git_add()
+    res , flag = gh.git_commit(msg)
+    #gh.git_push(branch)
+    if flag:
+        if "nothing to commit" in res:
+            return "nothing to commit"
+        else:
+            return "error"
+    else:
+
+        return "All good"
+
+def pull_editor(root,project_name,branch):
+    '''
+    Pull the data to php folder
+    send the directory structure to the user
+    '''
+    print(branch,project_name)
+    #Set the path and make the directory
+    fh = FHandler(root,make_dir=True)
+    print(fh.current_dir())
+    gh = GHandler(cwd = fh.current_dir())
+    #perform gti fetch
+    gh.git_init()
+    gh.git_remote_add(project_name = project_name)
+    #gh.add_ssh()
+    print(fh.current_dir())
+    gh.git_pull_server(branch = branch)
+    #msg, flag = gh.execute()
+    del gh
+    del fh
+    #Fetch the file structure
+    fh  = FHandler(root)
+
+    hierarchy = fh.get_hierarchy()
+    return hierarchy
+
+def save_commit(payload):
+    '''
+    {
+        branch: branch_name,
+        root: "root name",
+        commit_msg: "first commit",
+        files: [{
+                    filename: "test.txt",
+                    content:"test"
+                },
+                {
+                    filename: "test.txt",
+                    content:"test"
+                }]
+    }
+    '''
+    #payload = json.loads(payload1)
+    branch = payload["branch"]
+    commit_msg = payload["commit_msg"]
+    root = payload["root"]
+    files = payload["files"]
+    #Saving all the files
+    for file in files:
+        #path = file['path']
+        path = "/"
+        filename = file["filename"]
+        content = file["content"]
+        res = save_file(root=root,path = path,content = content, filename = filename)
+        if res == 1:
+            return "error saving"
+
+    # commiting the code
+    commit_res = commit(msg = commit_msg, branch = branch,root = root)
+    if commit_res == "error":
+        return "error commiting",1
+    if commit_res == "nothing to commit":
+        return "nothing to commit",1
+    return "done",0
+
+
+def save_file(root, path,content,filename):
+    fh = FHandler(root)
+    print(fh.current_dir())
+    #fh.set_default_path()
+    msg = fh.save_file(content = bytes(content,'utf-8'), file_name = filename,path = "/")
+    return msg
+
+def get_file(root,path,filename):
+    fh = FHandler(root)
+    print(fh.current_dir())
+    # if path!="/":
+    #     fh.change_dir(path)
+    #     print(fh.current_dir())
+    content = fh.get_file_content(filename=filename,path="/")
+    return content
+
+def make_dir(root,path,dir_name):
+    fh = FHandler(root)
+    # if path != '/':
+    #     fh.change_dir(path)
+    # print(fh.current_dir())
+    if fh.make_dir(dir_name) == 1:
+        return fh.get_hierarchy(),0
+    else:
+        return 0,1
+
+def make_file(root,path,filename):
+    fh = FHandler(root)
+    if fh.make_file(fname = filename,path=path) == 1:
+        return fh.get_hierarchy(),0
+    else:
+        return 0,1
+
+
+def make_repo(payload):
+    # send request to the git server using API
+    projectname = payload["projectname"] #project name in string
+    users = payload["users"]  #users in the form of array
+    users.append("dev")
+    if not os.path.isdir("/home/rohit/nfsfiles/git/"+projectname):
+        ps = subprocess.run(args = "git init --bare {projectname}.git".format(projectname = projectname), capture_output=True,shell=True,text=True,cwd="/home/rohit/nfsfiles/git")
+        print(ps)
+        fh = FHandler("temp_"+projectname, make_dir=True)
+        print("from the function: "+fh.current_dir())
+        gh = GHandler(fh.current_dir())
+        print(fh.setup_default_hierarchy())
+        # exit()
+        gh.git_init()
+        gh.git_remote_add(project_name=projectname)
+        gh.git_add()
+        gh.git_commit(msg="Initial Build-up")
+        for u in users:
+            gh.git_add_user(branch = u)
+        gh.git_push(branch = "master "+ " ".join(users))
+
+        fh.delete_folder_final()
+        return ("successfull", 0)
+    else:
+        return ("project already exist!",1)
+    
+    
+
+def delete_dir(root,path,dirname):
+    fh = FHandler(root)
+    fh.delete_dir(dirname = dirname)
+    return fh.get_hierarchy()
+
+
+def delete_file(root,path,filename):
+    fh = FHandler(root)
+    fh.delete_file(filename = filename)
+    return fh.get_hierarchy()
+
+def get_hir(root):
+    fh = FHandler(dir_name=root)
+    return fh.get_hierarchy()
+def add_user(payload):
+    try:
+        projectname = payload['projectname']
+        b_branch = payload['base_branch']
+        new_branches = ['new_branchs']
+        fh = FHandler("branch_"+projectname, make_dir=True)
+        gh = GHandler(fh.current_dir())
+        gh.git_init()
+        gh.git_remote_add(project_name=projectname)
+        gh.git_pull_server(branch = b_branch)
+        for n in new_branches:
+            gh.git_add_user(n)
+        gh.git_push(branch = " ".join(users))
+        fh.delete_folder_final()
+    except Exception as e:
+        return str(e)
+    return "Done"
+
+
+
+
+def tester(root):
+    t = time.time()
+    payload = {
+        "projectname":"first_test",
+        "users":["b1","b2","b3","b4","b5"]
+    }
+    make_repo(payload=payload)
+    print(time.time() - t)
+
+if __name__ == "__main__":
+    # hir = pull_editor(str(time.time()),"project","master")
+    # for files in hir:
+    #     print(files)
+    #ommit(msg,branch,parent_path)
+    #print(save_file(content = "Test file",root="dist", path='/',filename="test.txt"))
+    print(tester(root="test1"))
